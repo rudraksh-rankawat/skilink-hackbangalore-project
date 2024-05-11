8d7675c91700f41bd1a0dc800a19fd050eb14f82
yasharya2901 <yasharya2901@gmail.com>
2024-05-12 02:05:16 +0530

    Updated Exception

--- skiling-backend/src/main/java/com/github/hackbangalore/skilingbackend/exceptions/InvalidLinkedInAccount.java
+++ skiling-backend/src/main/java/com/github/hackbangalore/skilingbackend/exceptions/InvalidLinkedInAccount.java
@@ -1,14 +1,5 @@
 package com.github.hackbangalore.skilingbackend.exceptions;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class InvalidLinkedInAccount extends RuntimeException{
     public InvalidLinkedInAccount(String message) {
         super(message);
