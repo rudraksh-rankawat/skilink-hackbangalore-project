0ee40e289ba689217b11b537674e56246463bb07
yasharya2901 <yasharya2901@gmail.com>
2024-05-11 20:49:06 +0530

    User Controller

--- skiling-backend/src/main/java/com/github/hackbangalore/skilingbackend/services/FireBase.java
+++ skiling-backend/src/main/java/com/github/hackbangalore/skilingbackend/services/FireBase.java
@@ -4,9 +4,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 
+@Repository
 public class FireBase {
     public FireBase() throws Exception{
         ClassPathResource service = new ClassPathResource("serviceAccountKey.json");
